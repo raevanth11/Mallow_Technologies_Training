d33f03f1d1f646212179b78785cfc009be8bbd9e
raevanth11 <manoraevanth11@gmail.com>
2025-09-30 14:58:02 +0530

    demo

---
+++ example.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>hi</title>
+    </head>
+    <body>
+        <h1>good morning</h1>
+        <h2>happy morning</h2>
+    </body>
+    <?php
+    echo "just a try";
+    ?>
+</html>
