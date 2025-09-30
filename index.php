6368d6d8b87088e0dfd03b9c8948b5db17c398d2
raevanth11 <manoraevanth11@gmail.com>
2025-09-30 09:25:05 +0530

    Normalize line endings

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello</title>
+    </head>
+    <body>
+        
+    </body>
+    <?php
+      $d = date("l");
+      echo $d;
+      ?>
+</html>
