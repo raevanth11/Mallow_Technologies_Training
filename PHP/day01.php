b15b6c940eb3206e30b7fd95c7fb817544c3b08a
Raevanth M <168449258+raevanth11@users.noreply.github.com>
2025-09-30 09:17:39 +0530

    Add files via upload

---
+++ PHP/day01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+    HELLO
+</head>
+<body>
+    <h1>
+        Summa oru try
+    </h1>
+    <?php
+        $num = 8;
+        if($num % 2 == 0){
+            echo "Even number";
+        }else{
+            echo "Odd number";
+        }
+        ?>
+</body>
+</html>
